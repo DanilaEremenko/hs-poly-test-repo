2cd1578ad05a0370d3bb77c75caaa1602ac470e1
Danila <danilaeremenko@mail.ru>
2022-05-17 21:53:44 +0300

    Part1: most part of the Tasks.hs updated

--- src/Part1/Tasks.hs
+++ src/Part1/Tasks.hs
@@ -1,28 +1,80 @@
 module Part1.Tasks where
 
 import Util(notImplementedYet)
+import Data.List
+
+fact :: Double -> Double
+fact n
+    | n <=1     = 1
+    | otherwise = n * fact (n-1)
+
+
+taylorPrec = 50
 
 -- синус числа (формула Тейлора)
 mySin :: Double -> Double
-mySin = notImplementedYet
+mySin x = sinTaylorAcc x 0
+        where
+            sinTaylorAcc :: Double -> Double -> Double
+            sinTaylorAcc x acc
+                | acc == taylorPrec = sinTaylor x acc
+                | otherwise         = sinTaylor x acc + sinTaylorAcc x (acc + 1)
+                where
+                    sinTaylor :: Double -> Double -> Double
+                    sinTaylor x acc = ((-1) ** acc * x ** (2 * acc + 1)) / fact (2 * acc + 1)
+
 
 -- косинус числа (формула Тейлора)
 myCos :: Double -> Double
-myCos = notImplementedYet
+myCos x = cosTaylorAcc x 0
+        where
+            cosTaylorAcc :: Double -> Double -> Double
+            cosTaylorAcc x acc
+                | acc == taylorPrec = cosTaylor x acc
+                | otherwise         = cosTaylor x acc + cosTaylorAcc x (acc + 1)
+                where
+                    cosTaylor :: Double -> Double -> Double
+                    cosTaylor x acc = ((-1) ** acc * x ** (2 * acc)) / fact (2 * acc)
 
 -- наибольший общий делитель двух чисел
 myGCD :: Integer -> Integer -> Integer
-myGCD = notImplementedYet
+myGCD a b = myGCDAcc a b 1 1
+        where
+            myGCDAcc :: Integer -> Integer -> Integer -> Integer -> Integer
+            myGCDAcc a b currDivisor bestDivisor
+                | (a == 0) && (b == 0)                                          = 0
+                | (currDivisor > abs a) && (currDivisor > abs b)            = bestDivisor 
+                | ((a `mod` currDivisor) == 0) && ((b `mod` currDivisor) == 0)  = myGCDAcc a b (currDivisor + 1) currDivisor
+                | otherwise                                                     = myGCDAcc a b (currDivisor + 1) bestDivisor
+    
 
 -- является ли дата корректной с учётом количества дней в месяце и
--- вискокосных годов?
+-- вискокосных годов? 
 isDateCorrect :: Integer -> Integer -> Integer -> Bool
-isDateCorrect = notImplementedYet
+isDateCorrect day month year
+    | day <= 0                                         = False
+    | month == 2 && isYearLeap year       && day <= 29 = True
+    | month == 2 && not (isYearLeap year) && day <= 28 = True
+    | elem month [4, 6, 9, 7, 11]         && day <= 30 = True
+    | elem month [1, 3, 5, 8, 10, 12]     && day <= 31 = True
+    | otherwise                                        = False
+        where
+            isYearLeap :: Integer -> Bool
+            isYearLeap year = (mod year 4 == 0) && not (mod year 100 == 0) || (mod year 400 == 0)
+
 
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
 myPow :: Integer -> Integer -> Integer
-myPow = notImplementedYet
+myPow n pow
+    | pow == 0  = 1
+    | pow == 1  = n
+    | otherwise = myPowAcc n n 1 pow
+            where
+                myPowAcc :: Integer -> Integer -> Integer -> Integer -> Integer
+                myPowAcc n factor acc maxAcc
+                    | acc >= maxAcc  = n
+                    | otherwise      = myPowAcc (n * factor)  factor (acc + 1) maxAcc
 
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
