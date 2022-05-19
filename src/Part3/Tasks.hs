9eb1bc2a7e20216f9ee76b6ce05217375c3553fa
Danila <danilaeremenko@mail.ru>
2022-05-19 17:59:03 +0300

    Part3: mostFreq implemented

--- src/Part3/Tasks.hs
+++ src/Part3/Tasks.hs
@@ -1,7 +1,8 @@
 module Part3.Tasks where
 
 import Util (notImplementedYet)
-
+import Data.List
+import Data.Function
 -- Функция finc принимает на вход функцию f и число n и возвращает список чисел [f(n), f(n + 1), ...]
 finc :: (Int -> a) -> Int -> [a]
 finc f n = (f n) : finc f (n + 1)
@@ -12,8 +13,27 @@ ff :: (a -> a) -> a -> [a]
 ff f x = x : ff f (f x) 
 
 -- Дан список чисел. Вернуть самую часто встречающуюся *цифру* в этих числах (если таковых несколько -- вернуть любую)
+splitInt :: Int -> [Int]
+splitInt 0 = []
+splitInt x = x `mod` 10 : splitInt (x `div` 10)
+
 mostFreq :: [Int] -> Int
-mostFreq = notImplementedYet
+mostFreq lst = fst $ maximumBy (compare `on` snd) (freqHist $ concat $ map splitInt lst)
+    where 
+        filtNumber :: [Int] -> Int -> [Int]
+        filtNumber [] _ = []
+        filtNumber (x:xs) n
+            | x == n            = x : filtNumber xs n 
+            | otherwise         =     filtNumber xs n
+
+        countElPrt :: [Int] -> Int -> Int
+        countElPrt = (\lst -> (\n -> length (filtNumber lst n)))
+
+        freqOfList :: [Int] -> [Int] 
+        freqOfList lst = map (countElPrt lst) lst
+
+        freqHist :: [Int] -> [(Int, Int)]
+        freqHist lst = zip lst (freqOfList lst)
 
 -- Дан список lst. Вернуть список элементов из lst без повторений, порядок может быть произвольным.
 uniq :: (Eq a) => [a] -> [a]
