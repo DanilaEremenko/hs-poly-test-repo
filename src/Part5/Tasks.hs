a78e5420440bfe6b7be60992a2fa7cea7630fab4
Danila <danilaeremenko@mail.ru>
2022-05-19 20:36:35 +0300

    Part5: myFoldl & myFoldr added

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,11 +4,13 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl _ acc []     =  acc
+myFoldl f acc (x:xs) = myFoldl f (acc `f` x) xs  
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr _ acc []     =  acc  
+myFoldr f acc (x:xs) = f x $ myFoldr f acc xs
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
