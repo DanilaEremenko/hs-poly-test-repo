a2f3b2a5100e89ef2931a6602abb6748c0dda7c2
Danila <danilaeremenko@mail.ru>
2022-05-19 23:54:08 +0300

    Part5: myMap, myConcatMap, myConcat, myReverse added

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -10,24 +10,24 @@ myFoldl f acc (x:xs) = myFoldl f (acc `f` x) xs
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
 myFoldr _ acc []     =  acc  
-myFoldr f acc (x:xs) = f x $ myFoldr f acc xs
+myFoldr f acc (x:xs) = x `f` myFoldr f acc xs
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = myFoldr ((:) . f) []
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f lst = myFoldr ((++) . f) [] (myReverse lst)
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat lstlst = myFoldl (++) [] lstlst
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse lst = myFoldr (\x acc -> acc ++ [x]) [] lst
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter f lst = myFoldl (\acc x -> if f x then acc ++ [x] else acc) [] lst
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
 myPartition p = notImplementedYet
